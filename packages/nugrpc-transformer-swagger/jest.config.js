05761269e7252248cdfa063eeb13f34b75c4da24
Ade Novid <adenvt@gmail.com>
2022-03-01 18:50:03 +0700

    fix(nugrpc-api): wrong build script

--- packages/nugrpc-transformer-swagger/jest.config.js
+++ packages/nugrpc-transformer-swagger/jest.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   testEnvironment : 'node',
   moduleNameMapper: {
-    "/^@privyid\/google-rpc": '<rootDir>/google-rpc',
-    "/^@privyid\/(.*)$/"    : '<rootDir>/$1/src',
+    "/^@privyid/google-rpc": '<rootDir>/google-rpc',
+    "/^@privyid/(.*)$/"    : '<rootDir>/$1/src',
   },
   transform       : {
     '^.+\\.tsx?$': ['esbuild-jest', { sourcemap: true }]
