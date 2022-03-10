3a2b795dac5e6567e396fd8d7b75ad53760cd7ad
Ade Novid <adenvt@gmail.com>
2022-03-10 11:03:59 +0700

    refactor(rule-parser): using split instead of regex loop to avoid ReDOS

--- packages/nugrpc-rules-parser/src/index.ts
+++ packages/nugrpc-rules-parser/src/index.ts
@@ -1,7 +1,5 @@
 import { pascalCase } from "@privyid/nugrpc-utils"
 
-const RULE_COMMENT_REGEX = /^@rules:\s*?([\w-]+\s*?(?:\(.*?\))?(?:\s*?\|\s*?[\w-]+\s*?(?:\(.*?\))?)*)$/i
-
 const RULE_REGEX = /^([\w-]+)\s*?(?:\((.*)\))?$/
 
 const ESCAPE_CHAR = "\\"
@@ -49,9 +47,18 @@ export function isShortcutTag (text: string): boolean {
   })
 }
 
+export function isRulesTag(text: string): boolean {
+  const startTag = text.slice(0, 7).toLocaleLowerCase()
+  const rulesTag = text.slice(7).trim()
+
+  return startTag.startsWith('@rules:')
+    && rulesTag.length > 0
+    && !rulesTag.endsWith('|')
+    && splitBy(rulesTag, '|').every((rule) => RULE_REGEX.test(rule))
+}
+
 export function isValid (text: string): boolean {
-  return isShortcutTag(text)
-    || RULE_COMMENT_REGEX.test(text)
+  return isShortcutTag(text) || isRulesTag(text)
 }
 
 export function splitBy (params: string, delimiter: string): string[] {
@@ -117,13 +124,9 @@ export function parse (text: string): Rule[] {
     }]
   }
 
-  const syntax = text.match(RULE_COMMENT_REGEX)
-  const rules  = syntax?.[1]
+  if (!isRulesTag(text))
+    return []
 
-  if (rules) {
-    return splitBy(rules, '|')
-      .map((rule) => parseRule(rule))
-  }
-
-  return []
+  return splitBy(text.slice(7), '|')
+    .map((rule) => parseRule(rule))
 }
