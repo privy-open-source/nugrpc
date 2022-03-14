0c80410f984c919900fcb26ea6f3be8c37a7395f
Ade Novid <adenvt@gmail.com>
2022-03-14 10:15:26 +0700

    feat(playground): add nuxt 2 + bridge

--- playground/nuxt2-bridge/nuxt.config.ts
+++ playground/nuxt2-bridge/nuxt.config.ts
@@ -1,8 +1,10 @@
-module.exports = {
+import { defineNuxtConfig } from '@nuxt/bridge'
+
+export default defineNuxtConfig({
   buildModules    : ['@privyid/nugrpc-api-nuxt'],
   plugins         : [{ src: '@/plugins/axios' }],
   serverMiddleware: [
     { path: '/api/ping', handler: '@/api/ping' },
     { path: '/api/error', handler: '@/api/error' },
   ],
-}
+})
---
+++ playground/nuxt2-bridge/pages/error.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>Code: {{ code }}</h1>
+    <h2>Message: {{ message }}</h2>
+  </div>
+</template>
+
+<script>
+import { useApi, getCode, getMessage } from '@privyid/nugrpc-api'
+
+export default {
+  async asyncData() {
+    try {
+      await useApi().get('/api/error')
+    } catch (error) {
+      return {
+        code   : getCode(error),
+        message: getMessage(error),
+      }
+    }
+  }
+}
+</script>
