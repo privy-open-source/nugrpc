1a81894727edd12dc654d82bc87d9c98d5a0a03c
Ade Novid <adenvt@gmail.com>
2023-01-12 12:07:22 +0700

    build: remove nuxt2 and nuxt-bridge playground

--- playground/nuxt2/nuxt.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  server          : { port: 3000 },
-  buildModules    : ['@privyid/nugrpc-api-nuxt'],
-  plugins         : [{ src: '@/plugins/axios' }],
-  serverMiddleware: [
-    { path: '/api/ping', handler: '@/api/ping' },
-    { path: '/api/error', handler: '@/api/error' },
-  ],
-}
--- playground/nuxt2/plugins/axios.js
+++
@@ -1,8 +0,0 @@
-import { onRequest } from "@privyid/nugrpc-api"
-
-export default function extendApi ({ $api }) {
-  onRequest((config) => {
-    if (!config.headers['X-Custom-Header'])
-      config.headers['X-Custom-Header'] = 'This is custom header'
-  }, $api)
-}
